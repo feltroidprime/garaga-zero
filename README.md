c0056f975430c37d3fc4a99700d526e9581b6e2a
Oba <obatirou@gmail.com>
2025-04-18 16:04:08 +0200

    trunk fmt --all

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Cairo Zero version library of [garaga](https://github.com/keep-starknet-strange/garaga).
 Made for the most efficient local proving performance.
 
-
 ## Setup
+
 ```
 git clone https://github.com/feltroidprime/garaga-zero.git
 cd garaga-zero
@@ -13,14 +13,17 @@ source .venv/bin/activate
 ```
 
 ## Compile
+
 ```
 cairo-compile tests/fustat_programs/test_mpc_bls_2.cairo --proof_mode --cairo_path 'src' --output main.json
 ```
 
 ## Generate trace
+
 ```
 cairo-run --program main.json --layout=all_cairo --proof_mode --print_info --air_public_input public.json --air_private_input private.json
 ```
 
 ## Generate proof
+
 ...
--- tools/make/setup.sh
+++ tools/make/setup.sh
@@ -2,55 +2,55 @@
 
 # Function to install GNU parallel
 install_parallel() {
-    case "$OSTYPE" in
-        linux-gnu*)
-            # Linux
-            if command -v apt-get >/dev/null; then
-                # Debian/Ubuntu
-                sudo apt-get update && sudo apt-get install -y parallel
-            elif command -v dnf >/dev/null; then
-                # Fedora
-                sudo dnf install -y parallel
-            else
-                echo "Unsupported Linux distribution for automatic parallel installation."
-                exit 1
-            fi
-            ;;
-        darwin*)
-            # macOS
-            if command -v brew >/dev/null; then
-                brew install parallel
-            else
-                echo "Homebrew is not installed. Please install Homebrew and try again."
-                exit 1
-            fi
-            ;;
-        *)
-            echo "Unsupported operating system for automatic parallel installation."
-            exit 1
-            ;;
-    esac
+	case "$OSTYPE" in
+	linux-gnu*)
+		# Linux
+		if command -v apt-get >/dev/null; then
+			# Debian/Ubuntu
+			sudo apt-get update && sudo apt-get install -y parallel
+		elif command -v dnf >/dev/null; then
+			# Fedora
+			sudo dnf install -y parallel
+		else
+			echo "Unsupported Linux distribution for automatic parallel installation."
+			exit 1
+		fi
+		;;
+	darwin*)
+		# macOS
+		if command -v brew >/dev/null; then
+			brew install parallel
+		else
+			echo "Homebrew is not installed. Please install Homebrew and try again."
+			exit 1
+		fi
+		;;
+	*)
+		echo "Unsupported operating system for automatic parallel installation."
+		exit 1
+		;;
+	esac
 }
 
 # Check if parallel is installed, if not, attempt to install it
 if ! command -v parallel >/dev/null; then
-    echo "GNU parallel not found. Attempting to install..."
-    install_parallel
+	echo "GNU parallel not found. Attempting to install..."
+	install_parallel
 else
-    echo "GNU parallel is already installed."
+	echo "GNU parallel is already installed."
 fi
 
 # Create virtual environment
 if ! uv sync; then
-    echo "Failed to create virtual environment with uv"
-    exit 1
+	echo "Failed to create virtual environment with uv"
+	exit 1
 fi
 
 # Install the commit hooks (black, isort)
 pre-commit install
 
 echo "Applying patch to instances.py..."
-patch .venv/lib/python3.10/site-packages/starkware/cairo/lang/instances.py < tools/make/instances.patch
+patch .venv/lib/python3.10/site-packages/starkware/cairo/lang/instances.py <tools/make/instances.patch
 
 echo "WARNING: You need to apply the patch anytime you run uv sync again after this script"
 echo "All done!"
